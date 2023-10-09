6a2789089eb2ef6d69b94bf7d792f1ec7fee0e6c
felismario silva <33597850+felikis@users.noreply.github.com>
2023-10-09 13:14:35 -0300

    Add files via upload

---
+++ servidor/CadastraRadiosEmLote.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * esta classe deve receber a planilha do sistema interno para poder subir 
+ * para o servidor os dados dos equipamentos
+ * o campo fk ficara em branco e sera atualizado
+ * quando o usuario fizer alguma alteracao no radio como:programacao,
+ * correção de dados,etc
+ */
+
+ 
