4707f4bffc27504e787e38366656f0fb8cfb61a8
felismario silva <33597850+felikis@users.noreply.github.com>
2023-10-09 13:16:04 -0300

    Add files via upload

---
+++ servidor/LoginUsuario.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * recebe o usuario e realiza o cadastro na base
+ */
+include('Auxiliar.php');
+
+class LoginUsuario{
+    public function run(){
+        include('RespostaServidor.php');
+        include('conexao.php');
+        /*$usuario = json_decode($_POST['usuario']);
+        $senha = $usuario->_senha;
+        $query = "SELECT matricula FROM usuario WHERE matricula = '$usuario->_matricula'";
+        $row = mysqli_query($dbc,$query);
+        
+        if(mysqli_num_rows($row)){
+            $query = "INSERT INTO usuario (nomeUsuario,matricula,senha) VALUES ('$usuario->_nome','$senha','$usuario->_matricula')";
+            //mysqli_query($dbc,$query);
+
+            $resposta = new RespostaServidor();
+            $resposta->statusDaResposta = 'ok';
+        
+            mysqli_close($dbc);
+            echo json_encode($resposta);
+        return;
+        }*/
+        $resposta = new RespostaServidor();
+        $resposta->statusDaResposta = 'ok';
+        echo json_encode($resposta);
+    }
+}
